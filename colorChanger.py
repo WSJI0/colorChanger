ffac90d212935c818feccc2d73154be29f5a3e9c
WSJI0 <lifedev@naver.com>
2020-03-13 15:37:30 +0900

    Update colorChanger.py

--- colorChanger.py
+++ colorChanger.py
@@ -3,6 +3,7 @@ from tkinter import Button
 from tkinter import messagebox
 from tkinter import colorchooser
 from tkinter import *
+from math import floor
 import binascii
 import os
 
@@ -12,18 +13,18 @@ window.geometry("1600x900+160+90")
 window.resizable(False, False)
 
 def openFile():
-    global leng
+    global leng,newdir,mod
     filename=filedialog.askopenfilename(initialdir="C:/")
     with open(filename, 'rb') as f:
-        content = f.read()
+        content=f.read()
     mod=binascii.hexlify(content).decode()
 
     filenameArr=filename.split("/")
     filenameArr[len(filenameArr)-1]='temp.txt'
     newdir=str('/'.join(filenameArr))
-    newmod=open(newdir,'w')
-    newmod.write(mod)
-    newmod.close()
+    oldmod=open(newdir,'w')
+    oldmod.write(mod)
+    oldmod.close()
 
     if mod[0:16]=="89504e470d0a1a0a": #png signature
         if mod[mod.index("49484452")+26:mod.index("49484452")+28]=="03": #indexed-color
@@ -32,7 +33,7 @@ def openFile():
                 messagebox.showwarning("불러올 수 없음", "손상된 png 파일입니다.")
                 os.remove(newdir)
             else:
-                showColor(mod[mod.index("504c5445")+8:mod.index("504c5445")+8+leng])
+                showColor3(mod[mod.index("504c5445")+8:mod.index("504c5445")+8+leng])
         elif mod[mod.index("49484452")+26:mod.index("49484452")+28]=="02": #truecolor
             pass
         elif mod[mod.index("49484452")+26:mod.index("49484452")+28]=="04": #grayscale with alpha
@@ -48,35 +49,27 @@ def openFile():
     else:
         messagebox.showwarning("불러올 수 없음", "해당 파일은 (.png)형식의 파일이 아니거나 손상되었습니다.")
         os.remove(newdir)
-        
 
-menuBar=Menu(window)
-menu1=Menu(menuBar, tearoff=0)
-menu1.add_command(label="열기", command=openFile)
-menu1.add_separator()
-menu1.add_command(label="저장")
-menu1.add_command(label="다른 이름으로 저장")
-menuBar.add_cascade(label="파일", menu=menu1)
-
-menu2=Menu(menuBar, tearoff=0)
-menu2.add_command(label="버전 알림", command=lambda:messagebox.showwarning("정보", "미완성"))
-menuBar.add_cascade(label="정보", menu=menu2)
-
-window.config(menu=menuBar)
-
-
-def changeColor(o):
-    global fillArr,b
+def changeColor3(o):
+    global fillArr,b,plte
     color=colorchooser.askcolor(color=str("#")+str(fillArr[o][0])+str(fillArr[o][1])+str(fillArr[o][2]))
-    b[o]=Button(window, overrelief="flat", width=10, height=5, command=lambda c=o:changeColor(b[c].cget("text")), background=color[1], text=o)
+
+    plte[o*3]=str("{0:x}".format(floor(color[0][0])).zfill(2))
+    plte[o*3+1]=str("{0:x}".format(floor(color[0][1])).zfill(2))
+    plte[o*3+2]=str("{0:x}".format(floor(color[0][2])).zfill(2))
+
+    b[o]=Button(window, overrelief="flat", width=10, height=5, command=lambda c=o:changeColor3(b[c].cget("text")), background=color[1], text=o)
     b[o].grid(column=o%20,row=o//20)
 
-def confirmChange():
-    pass
-
+def confirmChange3():
+    global mod,newdir,plte
+    newmod=open(newdir,'w')
+    newmod.write(str(mod[0:mod.index("504c5445")+8])+str(''.join(plte))+str(mod[mod.index("49444154")-16:len(mod)]))
+    newmod.close()
+    
 b=[]
-def showColor(co):
-    global b,fillArr
+def showColor3(co):
+    global b,fillArr,plte
     plte=[]
     start=0
     while start!=leng:
@@ -92,9 +85,22 @@ def showColor(co):
     b=[]
     for i in range(len(fillArr)):
         color=str("#")+str(fillArr[i][0])+str(fillArr[i][1])+str(fillArr[i][2])
-        b.append(Button(window, overrelief="flat", width=10, height=5, command=lambda c=i:changeColor(b[c].cget("text")), background=color, text=i))
+        b.append(Button(window, overrelief="flat", width=10, height=5, command=lambda c=i:changeColor3(b[c].cget("text")), background=color, text=i))
         b[-1].grid(column=i%20,row=i//20)
-    confirm=Button(window, overrelief="flat", width=10, height=5, command=confirmChange)
-    confirm.grid(column=0,row=len(fillArr)//20+1)
+
+
+menuBar=Menu(window)
+menu1=Menu(menuBar, tearoff=0)
+menu1.add_command(label="열기", command=openFile)
+menu1.add_separator()
+menu1.add_command(label="저장", command=confirmChange3)
+menu1.add_command(label="다른 이름으로 저장")
+menuBar.add_cascade(label="파일", menu=menu1)
+
+menu2=Menu(menuBar, tearoff=0)
+menu2.add_command(label="버전 알림", command=lambda:messagebox.showwarning("정보", "미완성"))
+menuBar.add_cascade(label="정보", menu=menu2)
+
+window.config(menu=menuBar)
 
 window.mainloop()
